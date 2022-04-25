5b95f8f92ac3826b4689a5c52d83c3982910bf04
Giuseppe <77759603+gsardo@users.noreply.github.com>
2022-04-25 15:21:23 +1000

    Initial Commit

---
+++ HowLongIsAPieceOfString/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HowLongIsAPieceOfString
+//
+//  Created by Giuseppe Sardo on 25/4/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HowLongIsAPieceOfString/HowLongIsAPieceOfStringApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowLongIsAPieceOfStringApp.swift
+//  HowLongIsAPieceOfString
+//
+//  Created by Giuseppe Sardo on 25/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct HowLongIsAPieceOfStringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
